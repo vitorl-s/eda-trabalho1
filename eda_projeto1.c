14f10b40387057d1b188a9d476505975605411a9
Vitor Leal <vitinho2201@gmail.com>
2018-04-05 01:47:57 -0300

    Final trabalho1
    Signed-off-by: Vitor Leal <vitinho2201@gmail.com>

--- eda_projeto1.c
+++ eda_projeto1.c
@@ -1,12 +1,41 @@
 #include <stdio.h>
 
+void fneuronio(double *, double *, double, int, int *);
+
+int main(){
+
+  double limiar,entradas[10],pesos[10];
+  int ativavel;
+
+  for(int i=0;i<10;i++){
+    printf("Digite o valor da entrada %d: ", i+1 );
+    scanf("%lf", &entradas[i]);
+  }
+
+  for(int i=0;i<10;i++){
+    printf("Digite o valor do peso %d: ", i+1 );
+    scanf("%lf", &pesos[i]);
+  }
+  printf("Digite o valor do limiar: ");
+  scanf("%lf", &limiar);
+
+  fneuronio(entradas,pesos,limiar,10,&ativavel);
+
+  if(ativavel == 1)
+    printf("Neuronio ativado!\n");
+  else
+    printf("Neuronio desativado!\n");
+
+  return 0;
+}
+
 void fneuronio(double *entradas, double *pesos, double limiar, int tamanho, int *ativavel){
 
   double somap=0;
   int i=0;
 
   while(tamanho>0){
-    somap= (*(entradas+i))*(*(pesos+i));
+    somap += (*(entradas+i))*(*(pesos+i));
     tamanho--;
     i++;
   }
@@ -15,29 +44,4 @@ void fneuronio(double *entradas, double *pesos, double limiar, int tamanho, int 
   *ativavel=1;
   else
   *ativavel=0;
-}
-
-int main(){
-
-
-  double limiar,entradas[9],pesos[9];
-  int ativavel;
-
-  for(int i=0;i<3;i++)
-    scanf("%lf", &entradas[i]);
-
-  for(int i=0;i<3;i++)
-    scanf("%lf", &pesos[i]);
-
-  scanf("%lf", &limiar);
-
-
-  fneuronio(entradas,pesos,limiar,10,&ativavel);
-
-  if(ativavel == 1)
-    printf("Neuronio ativado!");
-  else
-    printf("Neuronio desativado!\n");
-
-  return 0;
 }
--- eda_projeto2.c
+++ eda_projeto2.c
@@ -4,6 +4,33 @@ void recebe_notas(double *, int, double *);
 void conta_notas(double*, int ,double *, double*);
 int percent_aprov(double *, double *, double *, double *);
 
+int main(){
+
+  double notas[10],apr[10],aprovados,reprovados,porcentagemAprov,porcentagemReprov;
+  int tamanho;
+
+  for(int i=0;i<10;i++){
+    printf("Digite a nota %d: ",i+1 );
+    scanf("%lf", notas+i);
+  }
+  recebe_notas(notas,10,apr);
+  conta_notas(apr,10,&aprovados,&reprovados);
+  percent_aprov(&aprovados,&reprovados,&porcentagemAprov,&porcentagemReprov);
+
+  printf("Quantidade de aprovados: %.1lf\n", aprovados );
+  printf("Quantidade de reprovados: %.1lf\n", reprovados );
+  printf("Percentagem de aprovados: %.2lf\n", porcentagemAprov );
+  printf("Percentagem de reprovados: %.2lf\n", porcentagemReprov );
+    if(percent_aprov(&aprovados,&reprovados,&porcentagemAprov,&porcentagemReprov) == 1)
+      printf("Mais da metade da turma foi aprovada!\n");
+    else if(porcentagemAprov == 50)
+      printf("Metade da turma foi aprovada!\n");
+    else
+      printf("Mais da metade da turma foi reprovada!\n");
+
+  return 0;
+}
+
 void recebe_notas(double *notas,int tamanho,double *apr){
   int i=0;
 
@@ -27,7 +54,7 @@ void conta_notas(double *apr,int tamanho,double *aprovados, double *reprovados){
       contadorAprovados++;
     }
     else
-      contadorReprovados++;
+    contadorReprovados++;
     tamanho--;
     i++;
   }
@@ -40,29 +67,8 @@ int percent_aprov(double *aprovados, double *reprovados, double *porcentagemApro
   *porcentagemAprov = (*aprovados/(*aprovados + *reprovados))*100;
   *porcentagemReprov = (*reprovados/(*aprovados + *reprovados))*100;
 
-    if(*porcentagemAprov > 50)
-      return 1;
-    else
-      return 0;
-}
-
-
-int main(){
-
-  double notas[10],apr[10],aprovados,reprovados,porcentagemAprov,porcentagemReprov;
-  int tamanho;
-
-  for(int i=0;i<10;i++)
-    scanf("%lf", notas+i);
-
-  recebe_notas(notas,10,apr);
-  conta_notas(apr,10,&aprovados,&reprovados);
-  percent_aprov(&aprovados,&reprovados,&porcentagemAprov,&porcentagemReprov);
-
-  printf("Quantidade de aprovados: %.1lf\n", aprovados );
-  printf("Quantidade de reprovados: %.1lf\n", reprovados );
-  printf("Percentagem de aprovados: %.2lf\n", porcentagemAprov );
-  printf("Percentagem de reprovados: %.2lf\n", porcentagemReprov );
-
+  if(*porcentagemAprov > 50)
+  return 1;
+  else
   return 0;
 }
